6b6256b0939c0a2d2ef014bb2cbe7cafba4c42c5
Felipe Cardozo <focardo@emory.edu>
2025-01-31 05:29:53 -1200

    Update FitnessTester.java

--- FitnessTester.java
+++ FitnessTester.java
@@ -1,4 +1,4 @@
-
+package HW.A1;
 
 /*
 THIS CODE WAS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
@@ -7,20 +7,22 @@ CODE WRITTEN BY OTHER STUDENTS OR COPIED FROM ONLINE RESOURCES. Philip Cardozo
 
 
 public class FitnessTester {
-    public static void main(String[] args) {
-        FitnessTracker tracker = new FitnessTracker();
 
-        tracker.addSteps(5000);//whats d diff FitnessTracker.addSteps(5000);
-        System.out.println(tracker.getTotalDistance());
-        tracker.addSteps(3000);System.out.println(tracker.getTotalDistance());
-        tracker.resetTracker();
-        System.out.println(tracker.getTotalDistance());
-        System.out.println(tracker.getProgress(1));
-        System.out.println(tracker.getCaloriesBurned());
-        System.out.println(tracker.getTotalSteps());
 
-    }
+public static void main(String[] args) {
+    FitnessTracker tracker = new FitnessTracker();
+
+    tracker.addSteps(5000);//whats d diff FitnessTracker.addSteps(5000);
+    System.out.println(tracker.getTotalDistance());
+    tracker.addSteps(3000);
+    System.out.println(tracker.getTotalDistance());
+    tracker.resetTracker();
+    System.out.println(tracker.getTotalDistance());
+    System.out.println(tracker.getProgress(1));
+    System.out.println(tracker.getCaloriesBurned());
+    System.out.println(tracker.getTotalSteps());
 
 
 }
 
+}

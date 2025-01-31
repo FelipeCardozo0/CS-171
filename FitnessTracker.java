e3b3b1e23974b9838a4f32e971506ecb60f625ea
Felipe Cardozo <focardo@emory.edu>
2025-01-31 05:09:21 -1200

    Update FitnessTracker.java

--- FitnessTracker.java
+++ FitnessTracker.java
@@ -1,6 +1,6 @@
 /*
 THIS CODE WAS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
-CODE WRITTEN BY OTHER STUDENTS OR COPIED FROM ONLINE RESOURCES. _Your_Name_Here_
+CODE WRITTEN BY OTHER STUDENTS OR COPIED FROM ONLINE RESOURCES. Philip Cardozo
 */
 
 public class FitnessTracker {
@@ -8,45 +8,47 @@ public class FitnessTracker {
     private double totalDistance;
     private double caloriesBurned;
 
-    public FitnessTracker() {
-        this.totalSteps = 0;
-        this.totalDistance = 0.0;
-        this.caloriesBurned = 0.0;
+    public FitnessTracker(){
+        totalSteps=0;
+        totalDistance=0;
+        caloriesBurned=0;
+
+    }
+    public static void main(String[] args) {
+
     }
 
-    public FitnessTracker(int totalSteps, double totalDistance, double caloriesBurned) {
-        this.totalSteps = totalSteps;
-        this.totalDistance = totalDistance;
-        this.caloriesBurned = caloriesBurned;
+    public void addSteps(int steps){//why static causes error
+        //adds steps to totalSteps, calculates the corresponding distance (assume 2,000 steps = 1 mile) and updates
+        // totalDistance, and calculates the calories burned (assume 1 mile burns 100 calories) and updates caloriesBurned
+        totalSteps++;
+        //(assume 2,000 steps = 1 mile)
     }
 
-    public void addSteps(int steps) {
-        totalSteps += steps;
-        double newDistance = steps / 2000.0;
-        totalDistance += newDistance;
-        caloriesBurned += newDistance * 100;
+    public double getTotalSteps(){
+        return (double) totalSteps /2000;
+
     }
 
-    public int getTotalSteps() {
-        return totalSteps;
+    public double getTotalDistance(){//miles
+        return (double) totalSteps /2000;
+
     }
 
-    public double getTotalDistance() {
-        return totalDistance;
+    public double getCaloriesBurned(){
+        //assume 1 mile burns 100 calories)
+        return (double)totalSteps/10;//1000*100
+
     }
 
-    public double getCaloriesBurned() {
-        return caloriesBurned;
+    public  void resetTracker(){//no return
+        totalSteps=0;
+        totalDistance=0;
+        caloriesBurned=0;
     }
 
-    public void resetTracker() {
-        totalSteps = 0;
-        totalDistance = 0.0;
-        caloriesBurned = 0.0;
-    }
+    public String getProgress(double goalDistance){
+        return "You have completed "+goalDistance/totalDistance*100+"% of your goal!";
 
-    public String getProgress(double goalDistance) {
-        double percentage = (totalDistance / goalDistance) * 100;
-        return String.format("You have completed %.1f%% of your goal.", percentage);
     }
 }

a9549b1bdcc67d0a4ea8f06db151a94174a29b46
Felipe Cardozo <focardo@emory.edu>
2025-02-01 10:05:10 +1400

    Update FitnessTracker.java

--- FitnessTracker.java
+++ FitnessTracker.java
@@ -1,23 +1,26 @@
+package HW.A1;
 
 /*
 THIS CODE WAS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
 CODE WRITTEN BY OTHER STUDENTS OR COPIED FROM ONLINE RESOURCES. Philip Cardozo
 */
 
+// Comments added while building the code as asked in the pdf and also to help me remember to ask questions to ta's
+
 public class FitnessTracker {
     private int totalSteps;
     private double totalDistance;
     private double caloriesBurned;
 
-    public FitnessTracker(){
+    public FitnessTracker(){//default
         totalSteps=0;
-        totalDistance=0.0;//Can it be 0?
+        totalDistance=0.0;//double
         caloriesBurned=0.0;
 
     }
 
     public     FitnessTracker(int totalSteps, double totalDistance,double caloriesBurned){
-        this.totalSteps=totalSteps;
+        this.totalSteps=totalSteps;//assign parameter to a instance variable, it gets updated to totalSteps
         this.totalDistance=totalDistance;
         this.caloriesBurned=caloriesBurned;
 
@@ -27,8 +30,8 @@ public class FitnessTracker {
         //adds steps to totalSteps, calculates the corresponding distance (assume 2,000 steps = 1 mile) and updates
         // totalDistance, and calculates the calories burned (assume 1 mile burns 100 calories) and updates caloriesBurned
         totalSteps+=steps;
-        totalDistance+= (double) steps /2000; //cant recalculate total distance based on  steps every time
-        caloriesBurned+= (double) steps /20;
+        totalDistance+= (double)steps/2000; //cant use totalSteps because then it calculates total distance based on steps every time
+        caloriesBurned+= (double) steps /2000*100;
 
     }
 

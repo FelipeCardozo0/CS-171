82a5ae7baa36835ed381cb8df93388a3f614f665
Felipe Cardozo <focardo@emory.edu>
2025-02-14 13:14:17 -0500

    Update ShapeTester.java

--- A2/ShapeTester.java
+++ A2/ShapeTester.java
@@ -1,29 +1,37 @@
-
-
 public class ShapeTester {
-
-    public static boolean isLarger(Circle c, Rectangle r) {
-        return c.getArea() >= r.getArea();
+    // Method to check if the area of the circle is larger than or equal to the area of the rectangle
+    public static boolean isLarger(Circle circle, Rectangle rectangle) {
+        return circle.getArea() >= rectangle.getArea();
     }
 
-    public static double longerPerim(Circle c, Rectangle r) {
-        return Math.max(c.getCircumference(), r.getPerimeter());
+    // Method to return the longer perimeter between a circle and a rectangle
+    public static double longerPerim(Circle circle, Rectangle rectangle) {
+        double circlePerimeter = circle.getCircumference();
+        double rectanglePerimeter = rectangle.getPerimeter();
+        return Math.max(circlePerimeter, rectanglePerimeter);
     }
 
-    public static double longerPerim(Rectangle r, Circle c) {
-        return longerPerim(c, r);
+    // Overloaded method to return the longer perimeter between a rectangle and a circle
+    public static double longerPerim(Rectangle rectangle, Circle circle) {
+        return longerPerim(circle, rectangle);
     }
 
-    public static double largerArea(Circle c, Rectangle r) {
-        return Math.max(c.getArea(), r.getArea());
+    // Method to return the larger area between a circle and a rectangle
+    public static double largerArea(Circle circle, Rectangle rectangle) {
+        return Math.max(circle.getArea(), rectangle.getArea());
     }
 
-    public static double largerArea(Rectangle r, Circle c) {
-        return largerArea(c, r);
+    // Overloaded method to return the larger area between a rectangle and a circle
+    public static double largerArea(Rectangle rectangle, Circle circle) {
+        return largerArea(circle, rectangle);
     }
 
-    public static boolean containsCenter(Circle c1, Circle c2) {
-        double[] center = c2.getCenter();
-        return c1.containsPoint(center[0], center[1]);
+    // Method to check if the first circle contains the center of the second circle
+    public static boolean containsCenter(Circle circle1, Circle circle2) {
+        double[] center1 = circle1.getCenter();
+        double[] center2 = circle2.getCenter();
+        double distanceSquared = Math.pow(center2[0] - center1[0], 2) + Math.pow(center2[1] - center1[1], 2);
+        double radiusSquared = Math.pow(circle1.getRadius(), 2);
+        return distanceSquared <= radiusSquared;
     }
 }
